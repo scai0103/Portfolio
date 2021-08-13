9bdff849b3cc6f8410ebdbcc9f51ba0d0a438b07
Sen Cai <caisen97@gmail.com>
2021-08-13 11:50:43 -0400

    v1.0

--- src/app.js
+++ src/app.js
@@ -15,7 +15,13 @@ app.get('/', function (req, res) {
       name: "Sen Cai",
       introduction: "My name is Sen Cai, a software engineer and web designer. " +
       "I have designed and improved MTA management system web client User Interface " +
-      "and built Passenager audio delivery system prototype for Staten Island Railroad"
+      "and built Passenager audio delivery system prototype for Staten Island Railroad", 
+      detail1: "Use Java to develop web applications for client server environments" +
+      "and add functionalities to existing applications", 
+      detail2: "Partnered with Staten island RailRoad clients to deeply understand their needs" +
+      "and build an audio distribution system using Java for customer.",
+      detail3: "Delivered improvements for our MTA client to their management system and" + 
+      "development environment to improve the code efficiency in AngularJs Development Environment."
   });
 })
 
